60a6bc2e7da57284198768870adcad5473e70df7
Ham 1414 <hamideshetu6@gmail.com>
2023-06-23 11:06:08 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: The destination of string
+ * @src: The source of string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
